28247abad310948f80b1bc2fabd276ce0255fa1d
Makmanu <vfrfv0640@gmail.com>
2025-05-09 21:24:45 +0300

    add func countLetters to stars.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from stats import  countWords
+import stats
 
 def getBookText(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    print(countWords(getBookText("books/frankenstein.txt")), "words found in the document")
+    frankenText = getBookText("books/frankenstein.txt")
+    print(stats.countWords(frankenText), "words found in the document")
+    print(stats.countLetters(frankenText))
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def countWords(string):
     return len(string.split())
+
+def countLetters(string):
+    lettersDict = {}
+    for letter in string:
+        letter = letter.lower()
+        if letter in lettersDict:     
+            lettersDict[letter] += 1
+        else:
+            lettersDict[letter] = 1
+    return lettersDict
